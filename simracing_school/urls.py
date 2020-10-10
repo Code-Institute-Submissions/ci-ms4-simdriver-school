5eb9b64bfc504f3053d25a5ab55084910b3360cc
milka77 <55999046+milka77@users.noreply.github.com>
2020-10-10 12:35:20 +0100

    added social media link to facebook in views and urls

--- simracing_school/urls.py
+++ simracing_school/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import Facebook
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('/facebook/', Facebook.as_view(), name="facebook"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- simracing_school/views.py
+++ simracing_school/views.py
@@ -3,7 +3,5 @@ from django.http import HttpResponse
 from django.views.generic.base import RedirectView
 
 
-def index(request):
-    """ A view to return the index page """
-
-    return render(request, 'home/index.html')
+class Facebook(RedirectView):
+    url = 'https://facebook.com'

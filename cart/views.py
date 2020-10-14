9d17b165f2988c06c0e51dbc33831ada490beb16
milka77 <55999046+milka77@users.noreply.github.com>
2020-10-14 18:10:22 +0100

    Added remove item from cart functionality

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -19,3 +19,18 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def remove_from_cart(request, item_id):
+    """ Remove a product from the shopping cart """
+
+    try:
+        cart = request.session.get('cart', {})
+
+        cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+        
+    except Exception as e:
+        return HttpResponse(status=500)

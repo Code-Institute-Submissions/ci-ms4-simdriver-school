df06257d07e72973e1dfca81812603f142e57cfe
milka77 <55999046+milka77@users.noreply.github.com>
2020-10-14 14:29:41 +0100

    added add to cart functionality

--- cart/views.py
+++ cart/views.py
@@ -6,3 +6,17 @@ def view_cart(request):
     """ A view to return the users shopping cart """
 
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """ Add a product to the shopping cart """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)

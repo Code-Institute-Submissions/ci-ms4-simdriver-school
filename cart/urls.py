66f1243b98e978dfb4b69bab14671c62abbe6d72
milka77 <55999046+milka77@users.noreply.github.com>
2020-11-02 19:09:21 +0000

    added new cart logic to prevent more single datapacks in the cart and offering gold pack to the customer

--- cart/urls.py
+++ cart/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.view_cart, name='view_cart'),
+    path('upgrade_cart/', views.upgrade_to_gold, name='upgrade_to_gold'),
     path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove/<item_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -19,11 +19,37 @@ def add_to_cart(request, item_id):
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
-    cart[item_id] = quantity
-    messages.success(request, f'Added {product.name} to your cart')
+    # Checking items in cart and offers the better deal
+    if cart == {'30': 1}:
+        messages.warning(request, f'You already have Gold Package in your cart. \
+            Gold Package gives you access to all the cars / datapacks')
+    elif cart != {}:
+        messages.info(request, 'You tried to add an another datapack to your cart. \
+            Why not upgrade your cart to the Gold Package (£9.99) to access all the cars for \
+            the same amount of money?')
+    else:
+        cart[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def upgrade_to_gold(request):
+    """
+    Upgrading the user's cart to Gold Package
+    """
+        
+    try:
+        if 'cart' in request.session:
+            del request.session['cart']
+
+            request.session['cart'] = {'30': 1}
+            messages.success(request, 'Your package was successfuly upgraded to Gold Package!')
+    except:
+        messages.error(request, 'Upgrade failed. Try again later.')
+
+    return redirect(reverse('view_cart'))
 
 
 def remove_from_cart(request, item_id):

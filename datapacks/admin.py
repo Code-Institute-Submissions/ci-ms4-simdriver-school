c662c44cf98c89d1ae65ee5b2095b30a0a6a5634
milka77 <55999046+milka77@users.noreply.github.com>
2020-11-08 11:17:35 +0000

    Customised the datapacks admin page

--- datapacks/admin.py
+++ datapacks/admin.py
@@ -2,6 +2,35 @@ from django.contrib import admin
 from .models import Week, Datapack, RaceTrack
 
 # Register your models here.
-admin.site.register(Week)
-admin.site.register(Datapack)
-admin.site.register(RaceTrack)
+
+
+class WeekAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+    ordering = ('id',)
+
+
+class RaceTrackAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+    ordering = ('name',)
+
+
+class DatapackAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'week',
+        'track_name'
+    )
+
+    ordering = ('product', 'week',)
+
+admin.site.register(Week, WeekAdmin)
+admin.site.register(Datapack, DatapackAdmin)
+admin.site.register(RaceTrack, RaceTrackAdmin)

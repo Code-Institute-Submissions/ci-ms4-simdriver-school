d5ff7a501fe8fd3a80d2743f97eb9c56bcc628aa
milka77 <55999046+milka77@users.noreply.github.com>
2020-11-11 19:44:02 +0000

    Removed secret key and set debug

--- simracing_school/settings.py
+++ simracing_school/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r+uj!&&@+mozszbt3lb2-dym58vh+a28$%z_%la$q9+)5p*en+'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ci-ms4-simracing-school.herokuapp.com', 'localhost', '127.0.0.1']
 
